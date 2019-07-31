b14cd90df331cb558d26085a9791b459a46e84be
Jan Jakubuv <jakubuv@gmail.com>
2019-07-31 20:25:53 +0200

    Simplify protocol naming convention

--- enigmatic/protos.py
+++ enigmatic/protos.py
@@ -1,20 +1,23 @@
 import re
 from pyprove import expres
 
-def solo(pid, name, mult=0, noinit=False, efun="Enigma"):
+def solo(pid, name, mult=0, noinit=False, efun="Enigma", fullname=False):
    proto = expres.protos.load(pid)
    enigma = "1*%s(PreferWatchlist,%s,%s)" % (efun, name, mult)
    eproto = "%s-H'(%s)'" % (proto[:proto.index("-H'")], enigma)
-   post = efun
-   post += ("0M%s" % mult) if mult else "0"
-   if noinit:
-      eproto = eproto.replace("--prefer-initial-clauses", "")
-      post += "No" 
-   epid = "Enigma+%s+%s+%s" % (name.replace("/","+"), pid, post)
+   if fullname:
+      post = efun
+      post += ("0M%s" % mult) if mult else "0"
+      if noinit:
+         eproto = eproto.replace("--prefer-initial-clauses", "")
+         post += "No" 
+      epid = "Enigma+%s+%s+%s" % (name.replace("/","+"), pid, post)
+   else:
+      epid = "Enigma+%s+solo" % name.replace("/","+")
    expres.protos.save(epid, eproto)
    return epid
 
-def coop(pid, name, freq=None, mult=0, noinit=False, efun="Enigma"):
+def coop(pid, name, freq=None, mult=0, noinit=False, efun="Enigma", fullname=False):
    proto = expres.protos.load(pid)
    post = efun
    if not freq:
@@ -25,10 +28,13 @@ def coop(pid, name, freq=None, mult=0, noinit=False, efun="Enigma"):
    post += ("M%s" % mult) if mult else ""
    enigma = "%d*%s(PreferWatchlist,%s,%s)" % (freq,efun,name,mult)
    eproto = proto.replace("-H'(", "-H'(%s,"%enigma)
-   if noinit:
-      eproto = eproto.replace("--prefer-initial-clauses", "")
-      post += "No"
-   epid = "Enigma+%s+%s+%s" % (name.replace("/","+"), pid, post)
+   if fullname:
+      if noinit:
+         eproto = eproto.replace("--prefer-initial-clauses", "")
+         post += "No"
+      epid = "Enigma+%s+%s+%s" % (name.replace("/","+"), pid, post)
+   else:
+      epid = "Enigma+%s+coop" % name.replace("/","+")
    expres.protos.save(epid, eproto)
    return epid
 
