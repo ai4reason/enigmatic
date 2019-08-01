3f54302031638b377f00e3531b388a1e72bfd174
Jan Jakubuv <jakubuv@gmail.com>
2019-08-01 03:46:02 +0200

    Add missing module

---
+++ enigmatic/learn/lgboosterext.py
@@ -0,0 +1,41 @@
+import subprocess
+import json
+from pyprove import log
+from .learner import Learner
+
+DEFAULTS = {
+   'max_depth': 9, 
+   'learning_rate': 0.3, 
+   'objective': 'binary', 
+   'num_round': 200,
+   'num_leaves': 300
+}
+
+BOOSTER_BIN = "enigmatic-lgbooster.py"
+
+class LightGBMExt(Learner):
+
+   def __init__(self, **args):
+      self.params = dict(DEFAULTS)
+      self.params.update(args)
+
+   def efun(self):
+      return "EnigmaLgb"
+
+   def ext(self):
+      return "lgb"
+
+   def name(self):
+      return "LightGBM"
+
+   def __repr__(self):
+      args = ["%s=%s"%(x,self.params[x]) for x in self.params]
+      args = ", ".join(args)
+      return "%s(%s)" % (self.name(), args)
+
+   def train(self, f_in, f_mod, f_log=None):
+      out = file(f_log, "a")
+      subprocess.call([BOOSTER_BIN, f_in, f_mod, json.dumps(self.params)],
+         stdout=out, stderr=subprocess.STDOUT)
+      out.close()
+
