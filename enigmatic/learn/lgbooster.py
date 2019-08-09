9f7d63d1970b0272b223282972a9b2d5f2d7d635
Jan Jakubuv <jakubuv@gmail.com>
2019-08-09 14:08:32 +0200

    Added cross-validation for LightGBM

--- enigmatic/learn/lgbooster.py
+++ enigmatic/learn/lgbooster.py
@@ -34,19 +34,18 @@ class LightGBM(Learner):
       args = ", ".join(args)
       return "%s(%s)" % (self.name(), args)
 
-   def validate(self, bst, data):
-      labels = data.get_label()
-      ipos = [i for (i,y) in enumerate(labels) if y == 1]
-      ineg = [i for (i,y) in enumerate(labels) if y == 0]
-      dpos = data.subset(ipos)
-      dneg = data.subset(ineg)
+   def validate(self, bst, xs, ys):
+      ipos = [i for (i,y) in enumerate(ys) if y == 1]
+      ineg = [i for (i,y) in enumerate(ys) if y == 0]
+      dpos = xs[ipos]
+      dneg = xs[ineg]
       ppos = bst.predict(dpos)
       pneg = bst.predict(dneg)
-      pos = len([x for x in ppos if x>0.5])
+      pos = len([x for x in ppos if x>=0.5])
       neg = len([x for x in pneg if x<0.5])
       return (pos, neg)
 
-   def train(self, f_in, f_mod, f_log=None, f_stats=None):
+   def train(self, f_in, f_mod, f_log=None, f_stats=None, test_size=0):
 
       def posneg(data):
          labels = data.get_label()
@@ -57,28 +56,34 @@ class LightGBM(Learner):
       stats = {}
 
       (xall, yall) = load_svmlight_file(f_in)
-      (xtrain, xtest, ytrain, ytest) = train_test_split(xall, yall, test_size=0.1, random_state=43)
-
-      print type(xtrain), type(ytest)
-
+      if test_size:
+         (xtrain, xtest, ytrain, ytest) = train_test_split(xall, yall, test_size=test_size, random_state=43)
+      else:
+         (xtrain, ytrain) = (xall, yall)
 
       dtrain = lgb.Dataset(xtrain, ytrain)
-      dtest = lgb.Dataset(xtest, ytest, reference=dtrain)
-       
       (pos, neg) = posneg(dtrain)
       self.params["scale_pos_weight"] = (float(neg)/float(pos))
       stats["train.pos.count"] = pos
       stats["train.neg.count"] = neg
 
-      (pos, neg) = posneg(dtest)
-      stats["test.pos.count"] = pos
-      stats["test.neg.count"] = neg
+      if test_size:
+         dtest = lgb.Dataset(xtest, ytest, reference=dtrain)
+         (pos, neg) = posneg(dtest)
+         stats["test.pos.count"] = pos
+         stats["test.neg.count"] = neg
 
-      bst = lgb.train(self.params, dtrain, valid_sets=[dtrain,dtest])
+      bst = lgb.train(self.params, dtrain, valid_sets=[dtrain,dtest] if test_size else [dtrain])
       bst.save_model(f_mod)
 
-      print self.validate(bst, dtrain)
+      (pos, neg) = self.validate(bst, xtrain, ytrain)
+      stats["train.pos.acc"] = pos / float(stats["train.pos.count"])
+      stats["train.neg.acc"] = neg / float(stats["train.neg.count"])
 
+      if test_size:
+         (pos, neg) = self.validate(bst, xtest, ytest)
+         stats["test.pos.acc"] = pos / float(stats["test.pos.count"])
+         stats["test.neg.acc"] = neg / float(stats["test.neg.count"])
 
       if f_stats:
          json.dump(stats, file(f_stats,"w"))
