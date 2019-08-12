5c7184a598d5ab56308c9de2e8b8044a938941a3
Jan Jakubuv <jakubuv@gmail.com>
2019-08-12 21:37:30 +0200

    Add XGBoost script

---
+++ bin/enigmatic-xgbooster.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+import json
+from enigmatic.learn import XGBoost
+
+if len(sys.argv) != 5:
+   print "usage: %s train.in model.out train.stats {json params}" % sys.argv[0]
+   sys.exit()
+
+f_in = sys.argv[1]
+f_mod = sys.argv[2]
+f_stats = sys.argv[3]
+params = json.loads(sys.argv[4])
+
+booster = XGBoost(**params)
+booster.train(f_in, f_mod, f_stats=f_stats)
+
