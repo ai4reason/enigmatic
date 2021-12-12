2f8c520b5d47a45353e8d32dc5618cf88592e519
Jan Jakubuv <jakubuv@gmail.com>
2021-12-12 20:30:31 +0100

    keep data balanced

--- enigmatic/trains.py
+++ enigmatic/trains.py
@@ -11,15 +11,17 @@ DEFAULT_DIR = os.getenv("PYPROVE_TRAINS", DEFAULT_NAME)
 
 logger = logging.getLogger(__name__)
 
-def name(bid, limit, features, dataname, split=False, forgets=(None,None), **others):
+def name(bid, limit, features, dataname, split=False, forgets=(None,None), balance=None, **others):
    tid = "%s-%s" % (bid.replace("/","-"), limit)
    trainname = features
    if split:
       trainname = "%s-spl%s" % (trainname, round(100*split))
-   if forgets != (None,None):
+   if balance is not None:
+      trainname = "%s-bal%s" % (trainname, balance)
+   elif forgets != (None,None):
       fneg = round(100*forgets[0]) if forgets[0] != None else "None"
       fpos = round(100*forgets[1]) if forgets[1] != None else "None"
-      trainname = "%s-for%sget%s" % (trainname, fneg, fpos)
+      trainname = "%s-frgt%s:%s" % (trainname, fneg, fpos)
    return os.path.join(tid, dataname, trainname)
 
 def path(**others):
@@ -74,8 +76,8 @@ def compress(f_in):
    numpy.savez_compressed(z_label, label=label)
    logger.debug("- compressed size: %s" % human.humanbytes(size(f_in)))
 
-def forgetting(lines, forget):
-   if forget is None:
+def forgetting(lines, forget, posbal):
+   if (forget is None) and (posbal is None):
       return lines
    def duplicates(line):
       nonlocal cache, lines
@@ -85,10 +87,17 @@ def forgetting(lines, forget):
    
    lines = lines.decode()
    lines = lines.strip().split("\n")
-   if forget:
+   if forget or posbal:
       cache = {}
       [duplicates(l) for l in lines] 
       lines = sorted(set(lines), key=duplicates, reverse=True)
+      if posbal:
+         (poscount, balance) = posbal
+         negcount = len(lines)
+         if negcount > balance * poscount:
+            forget = (negcount - balance * poscount) / negcount
+         else:
+            forget = 0
       idx = int((1.0-forget)*len(lines))
       idx = min(len(lines),max(1,idx))
       lines = lines[:idx]
@@ -98,7 +107,7 @@ def forgetting(lines, forget):
    lines = lines.encode()
    return lines
 
-def makesingle(f_list, features, f_problem=None, f_map=None, f_buckets=None, f_out=None, prefix=None, forget=0.0):
+def makesingle(f_list, features, f_problem=None, f_map=None, f_buckets=None, f_out=None, prefix=None, forget=0.0, posbal=None):
    args = [
       "enigmatic-features", 
       "--free-numbers", 
@@ -121,25 +130,34 @@ def makesingle(f_list, features, f_problem=None, f_map=None, f_buckets=None, f_o
       out = subprocess.check_output(args)
    except subprocess.CalledProcessError as e:
       return None
-   out = forgetting(out, forget)
+   out = forgetting(out, forget, posbal)
    if f_out:
       with open(f_out, "ab") as f: f.write(out)
    return out
 
-def makes(posnegs, f_name, bid, features, cores, msg="[+/-]", d_info=None, options=[], debug=[], batchsize=None, forgets=(None,None), **others):
-   others = dict(others, bid=bid, features=features, cores=cores, options=options, debug=debug, batchsize=batchsize, forgets=forgets)
+def makes(posnegs, f_name, bid, features, cores, msg="[+/-]", d_info=None, options=[], debug=[], batchsize=None, forgets=(None,None), balance=None, **others):
+   others = dict(others, bid=bid, features=features, cores=cores, options=options, debug=debug, batchsize=batchsize, forgets=forgets, balance=balance)
    def job(f_list):
+      nonlocal pos_count
       p = os.path.basename(f_list)[:-4]
       pos = f_list.endswith(".pos")
       f_problem = expres.benchmarks.path(bid, p)
       f_map = os.path.join(d_info, p+".map") if d_info else None
       f_buckets  = os.path.join(d_info, p+".json") if d_info else None
       f_out = os.path.join(d_info, p+".in") if d_info else None
+      posbal = None
+      if (not pos) and balance:
+         f_pos = f_list[:-4]+".pos"
+         if f_pos in pos_count:
+            posbal = (pos_count[f_pos], balance)
       forget = forgets[int(pos)]
-      return (f_list, features, f_problem, f_map, f_buckets, f_out, pos, forget)
-   def save(res, bar):
-      nonlocal out, part, written, parts, f_in
+      return (f_list, features, f_problem, f_map, f_buckets, f_out, pos, forget, posbal)
+   def save(job, res, bar):
+      nonlocal out, part, written, parts, f_in, pos_count
       if not res: return
+      f_list = job[0]
+      if f_list.endswith(".pos"):
+         pos_count[f_list] = res.count(b"\n")
       if written and batchsize and written >= batchsize:
          #dump(out, f_in)
          out.close()
@@ -158,12 +176,22 @@ def makes(posnegs, f_name, bid, features, cores, msg="[+/-]", d_info=None, optio
    f_in = filename(f_name, part, **others)
    os.system('mkdir -p "%s"' % os.path.dirname(f_in))
    with open(filename("%s-posnegs.txt"%f_name,**others),"w") as f: f.write("\n".join(posnegs))
-   jobs = list(map(job, posnegs))
-   barmsg = msg if not "headless" in options else None
+   # prepare
+   pos_count = {}
+   poss = [x for x in posnegs if x.endswith(".pos")]
+   negs = [x for x in posnegs if x.endswith(".neg")]
    parts = [f_in]
    out = open(f_in, "wb")
    written = 0
+   # positives
+   jobs = list(map(job, poss))
+   barmsg = msg+"(+)" if not "headless" in options else None
    par.apply(makesingle, jobs, cores=cores, barmsg=barmsg, callback=save, chunksize=100)
+   # negatives
+   jobs = list(map(job, negs))
+   barmsg = msg+"(-)" if not "headless" in options else None
+   par.apply(makesingle, jobs, cores=cores, barmsg=barmsg, callback=save, chunksize=100)
+   # finish
    out.close()
    if not "nozip" in debug:
       for f in parts:
